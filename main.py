1214288efe3dd0efbcdd0a3629c0eed370da6436
Максим Шевелев <maksimka9331234@gmail.com>
2023-01-05 14:48:21 +0500

    code according to PEP81

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def main():
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
-    print('Воитель, Маг, Лекарь')
+    print('Воительница, Маг, Лекарь')
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
 
